c0a4c94e469e765e751947d3897c13aae45c1450
aureliodeboa <aurelio74123@gmail.com>
2025-02-10 10:25:41 -0300

    fix: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,6 @@ import { ThemeProvider } from '@/contexts/ThemeContext';
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
     <ThemeProvider>
     {isLoggedIn ? (
